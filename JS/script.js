3314e87c26bd706bbca31bb48198deae33272407
Johan Abel Taveras <1988jnts@gmail.com>
2025-09-06 08:15:23 -0400

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -28,7 +28,7 @@ const myFavoriteFootballTeam = {
       isCaptain: false,
       nickname: null,
     },
-    {
+     {
       name: "Ricardo Bochini",
       position: "midfielder",
       number: 3,
@@ -181,13 +181,22 @@ worldCupYear.textContent = year;
 headCoach.textContent = coachName;
 
 const setPlayerCards = (arr = players) => {
+
+
   playerCards.innerHTML += arr.map(
     ({ name, position, number, isCaptain, nickname }) => {
-      return `
+     return `
         <div class="player-card">
           <h2>${isCaptain ? "(Captain)" : ""} ${name}</h2>
+          <p>Position: ${position}</p>
+          <p>Number: ${number}</p>
+          <p>Nickname: ${nickname !== null ? nickname : "N/A"}</p>
         </div>
       `;
     }
-  );
+  ).join("")
+
+
 };
+
+
